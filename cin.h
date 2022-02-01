e7adebe6a940fa86899f86030455a045011bd905
titi_2115 <73474137+jasiukiewicztymon@users.noreply.github.com>
2022-02-01 19:02:35 +0100

    Create cin.h

---
+++ cin.h
@@ -0,0 +1,10 @@
+/*
+Cin return the string to cin but during the cin can help the user to do his choice
+
+*/
+#include <string>
+#include <vector>
+#include <map>
+#include <Windows.h>
+
+std::string cin(std::vector<std::string>, std::map<std::string, std::vector<std::string>>);

ab43cc7469bb9e53978e1ffee087c2e20adcb249
titi_2115 <73474137+jasiukiewicztymon@users.noreply.github.com>
2022-02-06 20:07:55 +0100

    Update cin.h

--- cin.h
+++ cin.h
@@ -7,4 +7,4 @@ Cin return the string to cin but during the cin can help the user to do his choi
 #include <map>
 #include <Windows.h>
 
-std::string cin(std::vector<std::string>, std::map<std::string, std::vector<std::string>>);
+void bcin(std::string&, std::string, std::vector<std::vector<std::string>>);

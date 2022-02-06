18f16104906dcbf1e0afed6f52d62502b49e9e8b
titi_2115 <73474137+jasiukiewicztymon@users.noreply.github.com>
2022-02-06 21:20:56 +0100

    Update cin.h

--- cin.h
+++ cin.h
@@ -1,10 +1,1 @@
-/*
-Cin return the string to cin but during the cin can help the user to do his choice
-
-*/
-#include <string>
-#include <vector>
-#include <map>
-#include <Windows.h>
-
 void bcin(std::string&, std::string, std::vector<std::vector<std::string>>);
